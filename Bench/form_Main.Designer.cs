6ca3a1868ef01edfeabe15bad5e518311b0309f0
provsecbub <bobrien@provensecure.com>
2013-08-27 20:29:08 -0400

    Sprint 3 with icon
    Restore the ProvenSecure icon to the project

--- Bench/form_Main.Designer.cs
+++ Bench/form_Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
             this.panelHeader = new System.Windows.Forms.Panel();
             this.panelHeaderAnalyze = new System.Windows.Forms.Panel();
             this.ll_Analyze = new System.Windows.Forms.LinkLabel();
@@ -246,6 +247,7 @@
             this.Controls.Add(this.panelBody);
             this.Controls.Add(this.panelHeader);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "formMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
